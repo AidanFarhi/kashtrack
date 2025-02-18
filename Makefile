31a2c3de90fe92ce8b480fc7bed1cab90c504ac1
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-02-18 17:02:16 -0500

    added steps to open and close sg

--- Makefile
+++ Makefile
@@ -1,16 +1,27 @@
 
+.PHONY: open_firewall
+open_firewall:
+	@./scripts/open_firewall.sh
+
+.PHONY: close_firewall
+close_firewall:
+	@./scripts/close_firewall.sh
+
 .PHONY: startserver
 startserver:
+	@./scripts/open_firewall.sh
 	@./scripts/start_server.sh
+	@./scripts/close_firewall.sh
 
 .PHONY: stopserver
 stopserver:
+	@./scripts/open_firewall.sh
 	@./scripts/stop_server.sh
-
-.PHONY: migratedb
-migratedb: startserver
-	@./scripts/migrate_db.sh
+	@./scripts/close_firewall.sh
 
 .PHONY: deploy
-deploy: startserver
+deploy:
+	@./scripts/open_firewall.sh
+	@./scripts/start_server.sh
 	@./scripts/deploy.sh
+	@./scripts/close_firewall.sh
---
+++ scripts/close_firewall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "setting secrets"
+source ./scripts/init_secrets.sh
+
+echo "getting instance ID"
+instance_id=$( \
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=kashtrack-server" \
+        --query "Reservations[*].Instances[*].InstanceId" \
+        --output text \
+)
+
+echo "retrieving security group ID"
+security_group_id=$( \
+    aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query "Reservations[*].Instances[*].SecurityGroups[*].GroupId" \
+        --output text \
+)
+
+echo "fetching current public IP"
+my_ip=$(curl -s https://checkip.amazonaws.com)
+
+echo "removing SSH access from $my_ip"
+aws ec2 revoke-security-group-ingress \
+    --group-id $security_group_id \
+    --protocol tcp \
+    --port 22 \
+    --cidr ${my_ip}/32 \
+    > /dev/null 2>&1
+
+echo "security group rule removed successfully"
